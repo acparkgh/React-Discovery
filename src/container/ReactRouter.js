465da49af7a1f3d60c76eaad8769067a9b67a5b7
acparkgh <acpark123@gmail.com>
2021-02-19 17:43:18 -0500

    added NavLinks and Route Paths

--- src/container/ReactRouter.js
+++ src/container/ReactRouter.js
@@ -2,25 +2,28 @@ import './ReactRouter.css';
 import React, { Component } from 'react';
 import { Route, NavLink } from 'react-router-dom';
 
-
 class ReactRouter extends Component {
-
-
 
   render() {
     return (
       <div>
         <nav>
-          <NavLink to='/'>Home</NavLink>
-          <NavLink to='/Summary'>Summary</NavLink>
-          <NavLink to='/Body'>Body</NavLink>
-          <NavLink to='/Body/Section'>Section</NavLink>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/Introduction">Introduction</NavLink>
+          <NavLink to="/Summary">Summary</NavLink>
+          <NavLink to="/Body">Body</NavLink>
+          <NavLink to="/Body/Index">Index</NavLink>
+          <NavLink to="/Body/Page1">Page1</NavLink>
+          <NavLink to="/Body/Page2">Page2</NavLink>
+          <NavLink to="/Body/Page3">Page3</NavLink>
         </nav>
 
-        <Route path='/' render={ () => <h2>Home</h2> } />
-        <Route path='/Summary' render={ () => <h3>Summary</h3> } />
-        <Route path='/Body' render={ () => <h3>Body</h3> } />
-        <Route path='/Body/Section' render={ () => <p>Section</p> } />
+        <Route path="/" render={ () => <h2>Home</h2> } />
+        <Route path="/Introduction" render={ () => <h2>Introduction</h2> } />
+        <Route path="/Summary" render={ () => <h2>Summary</h2> } />
+        <Route path="/Body" render={ () => <h2>Body</h2> } />
+        <Route path="/Body/:PageNum" render={ () => <p>Page</p> } />
+        <Route path="/Body/Index" render={ () => <h3>Index</h3> } />
         
       </div>
     )
