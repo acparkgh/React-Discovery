9ffa0ce275e4845a513eee0ec80c6a37be24b0b7
acparkgh <acpark123@gmail.com>
2021-04-11 04:34:00 -0400

    clean up code

--- src/component/constant.js
+++ src/component/constant.js
@@ -2,16 +2,16 @@ import React from 'react';
 
 const Constant = () => {
 
-  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const arr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ] ;
   
-  const evenNumbers = arr.filter( num => num % 2 === 0 );    
+  const evenNumbers = arr.filter( num => num % 2 === 0 );
   const squared = evenNumbers.map( num => num * 2 );
    
   return (
     <div>
-      { squared.map( num => <button>{num}</button> ) } 
+      { squared.map( num => <button>{num}</button> ) }
     </div>
-  )
-}
+  );
+};
 
 export default Constant;
